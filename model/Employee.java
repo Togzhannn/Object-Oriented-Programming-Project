f76e2f2d602b1dabf31c3df3ba4f90e19c4659d5
Togzhan Razuan <togzhanj07@gmail.com>
2026-05-17 15:47:30 +0500

    updates

--- model/Employee.java
+++ model/Employee.java
@@ -1,14 +1,18 @@
 package model;
 
 import util.SystemLogger;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Employee extends User {
+public abstract class Employee extends User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private String department;
     private double salary;
     private List<Message> inbox = new ArrayList<>();
+    private List<Request> requests = new ArrayList<>();
 
     public Employee(int id, String firstName, String lastName,
                     String email, String password,
@@ -31,6 +35,13 @@ public abstract class Employee extends User {
         System.out.println("Complaint sent from " + getFullName() + " to manager " + to.getFullName());
     }
 
+    public void sendRequest(String description) {
+        Request request = new Request(this, description);
+        requests.add(request);
+        SystemLogger.getInstance().log("REQUEST: " + getFullName() + " submitted a request: " + description);
+        System.out.println("Request submitted by " + getFullName());
+    }
+
     public void receiveMessage(Message message) {
         inbox.add(message);
     }
@@ -50,4 +61,5 @@ public abstract class Employee extends User {
     public double getSalary()             { return salary; }
     public void setSalary(double salary)  { this.salary = salary; }
     public List<Message> getInbox()       { return inbox; }
+    public List<Request> getRequests()    { return requests; }
 }
