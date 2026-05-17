3bbd1fa482881bf03b160009586503693342219a
Sharipova Moldyr <mo_sharipova@kbtu.kz>
2026-05-18 03:18:00 +0600

    fh

--- model/User.java
+++ model/User.java
@@ -61,15 +61,35 @@ public abstract class User implements Serializable {
         return Objects.hash(id, email);
     }
 
-    public int getId()                       { return id; }
-    public void setId(int id)                { this.id = id; }
-    public String getFirstName()             { return firstName; }
-    public void setFirstName(String fn)      { this.firstName = fn; }
-    public String getLastName()              { return lastName; }
-    public void setLastName(String ln)       { this.lastName = ln; }
-    public String getEmail()                 { return email; }
-    public void setEmail(String email)       { this.email = email; }
-    public String getPassword()              { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public int getId(){ 
+        return id; 
+    }
+    public void setId(int id){
+        this.id = id; 
+    }
+    public String getFirstName(){ 
+        return firstName; 
+    }
+    public void setFirstName(String fn){
+        this.firstName = fn; 
+    }
+    public String getLastName(){ 
+        return lastName; 
+    }
+    public void setLastName(String ln){ 
+        this.lastName = ln; 
+    }
+    public String getEmail(){ 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
+    public String getPassword(){ 
+        return password; 
+    }
+    public void setPassword(String password){
+        this.password = password; 
+    }
 }
 
