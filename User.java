e06730b358c376800a36b0c843a3561daf140c9f
Togzhan Razuan <togzhanj07@gmail.com>
2026-05-17 15:03:50 +0500

    User class with login and logout methods

---
+++ User.java
@@ -0,0 +1,69 @@
+package model;
+
+import exceptions.InfoNotCorrectException;
+import util.SystemLogger;
+import java.util.Objects;
+
+public abstract class User {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(int id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void login(String inputEmail, String inputPassword) throws InfoNotCorrectException {
+        if (!this.email.equals(inputEmail) || !this.password.equals(inputPassword)) {
+            SystemLogger.getInstance().log("FAILED LOGIN attempt for email: " + inputEmail);
+            throw new InfoNotCorrectException("Invalid email or password for: " + inputEmail);
+        }
+        SystemLogger.getInstance().log("LOGIN: " + getFullName() + " (id=" + id + ")");
+        System.out.println("Welcome, " + getFullName() + "!");
+    }
+
+    public void logout() {
+        SystemLogger.getInstance().log("LOGOUT: " + getFullName() + " (id=" + id + ")");
+        System.out.println(getFullName() + " has logged out.");
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{id=" + id + ", name='" + getFullName() + "', email='" + email + "'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+
+    public int getId()                       { return id; }
+    public void setId(int id)                { this.id = id; }
+    public String getFirstName()             { return firstName; }
+    public void setFirstName(String fn)      { this.firstName = fn; }
+    public String getLastName()              { return lastName; }
+    public void setLastName(String ln)       { this.lastName = ln; }
+    public String getEmail()                 { return email; }
+    public void setEmail(String email)       { this.email = email; }
+    public String getPassword()              { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
