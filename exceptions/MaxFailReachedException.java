7e0abb1509bf7811c1ac4a278dede277c7a0f683
Dilyara <amankossova.dilyara@gmail.com>
2026-05-17 15:56:59 +0300

    exception

--- exceptions/MaxFailReachedException.java
+++ exceptions/MaxFailReachedException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
-public class MaxFailReachedException {
+import java.io.Serializable;
 
+public class MaxFailReachedException extends Exception implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public MaxFailReachedException(String message) { 
+    	super(message);
+    }
 }
