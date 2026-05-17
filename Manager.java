42b5a225018a2a5eed86b1be1ee12e876c7feb53
Togzhan Razuan <togzhanj07@gmail.com>
2026-05-17 15:01:57 +0500

    Add Manager class

---
+++ Manager.java
@@ -0,0 +1,23 @@
+package model;
+
+import enums.ManagerType;
+
+public class Manager extends Employee {
+
+    private ManagerType managerType;
+
+    public Manager(int id, String firstName, String lastName,
+                   String email, String password,
+                   String department, double salary,
+                   ManagerType managerType) {
+        super(id, firstName, lastName, email, password, department, salary);
+        this.managerType = managerType;
+    }
+
+    public ManagerType getManagerType() { return managerType; }
+
+    @Override
+    public String toString() {
+        return "Manager{name='" + getFullName() + "', type=" + managerType + "}";
+    }
+}
