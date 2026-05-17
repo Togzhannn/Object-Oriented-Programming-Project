ba1db46c0aa523144200403232d70aec7e5a9386
Dilyara <amankossova.dilyara@gmail.com>
2026-05-17 16:16:03 +0300

    MaxFail0Wxception

--- exceptions/MaxFailReachedException.java
+++ exceptions/MaxFailReachedException.java
@@ -5,6 +5,6 @@ import java.io.Serializable;
 public class MaxFailReachedException extends Exception implements Serializable {
     private static final long serialVersionUID = 1L;
     public MaxFailReachedException(String message) { 
-    	super(message);
+    		super(message);
     }
 }
