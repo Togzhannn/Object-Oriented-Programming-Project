478ebf433b7f2dd54fb22005bf9386156386b541
Dilyara <amankossova.dilyara@gmail.com>
2026-05-18 02:37:25 +0300

    comment

--- model/Message.java
+++ model/Message.java
@@ -1,22 +1,32 @@
 package model;
 
 import java.io.Serializable;
-
+/**
+  Represents a message between two employees in the system.
+  Message contains:
+  sender, receiver, text content and read status.
+ */
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    //Sender of the message
     private Employee sender;
+    // Receiver of the message
     private Employee receiver;
     private String content;
     private boolean isRead;
-
+    /**
+     Creates a new message.
+     @param sender message sender
+     @param receiver message receiver
+     @param content message text
+     */
     public Message(Employee sender, Employee receiver, String content) {
         this.sender = sender;
         this.receiver = receiver;
         this.content = content;
         this.isRead = false;
     }
-
+    //Marks message as read.Updates read status and prints confirmation message.
     public void mark() {
         this.isRead = true;
         System.out.println("Message from " + sender.getFullName() + " marked as read.");
