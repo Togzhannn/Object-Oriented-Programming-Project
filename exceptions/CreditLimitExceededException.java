002427d5d80ca16c803f642847ce75eade61ec0c
Dilyara <amankossova.dilyara@gmail.com>
2026-05-17 16:14:53 +0300

    CreditLimitException

--- exceptions/CreditLimitExceededException.java
+++ exceptions/CreditLimitExceededException.java
@@ -4,5 +4,7 @@ import java.io.Serializable;
 
 public class CreditLimitExceededException extends Exception implements Serializable {
     private static final long serialVersionUID = 1L;
-    public CreditLimitExceededException(String message) { super(message); }
+    public CreditLimitExceededException(String message) {
+    	super(message); 
+    	}
 }
